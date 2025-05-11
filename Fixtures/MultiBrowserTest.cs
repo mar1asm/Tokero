af161dee0f7e32604272047c9c45cf9e38eda942
Maria <mar1asm@yahoo.com>
2025-05-11 20:16:44 +0300

    Multi-browser testing initial setup
    Add support for running tests across Chromium, Firefox, and Webkit

---
+++ Fixtures/MultiBrowserTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal.Builders;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokero.Fixtures
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class MultiBrowserTest : NUnitAttribute, ITestBuilder
+    {
+        public IEnumerable<TestMethod> BuildFrom(IMethodInfo method, Test suite)
+        {
+            foreach (BrowserTypeEnum browser in Enum.GetValues(typeof(BrowserTypeEnum)))
+            {
+                var parameters = new TestCaseParameters(new object[] { browser });
+
+                var testMethod = new NUnitTestCaseBuilder().BuildTestMethod(method, suite, parameters);
+
+                testMethod.Name = $"{method.Name}_{browser}";
+
+                yield return testMethod;
+            }
+        }
+    }
+}
---
+++ Fixtures/PlaywrightTestFixtures.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Tokero.Fixtures;
+
+namespace TokeroTests.Fixtures
+{
+    public class PlaywrightTestFixture
+    {
+        protected IBrowser? Browser;
+        protected IPage? Page;
+        protected IBrowserContext? Context;
+        protected IPlaywright? Playwright;
+        private BrowserTypeEnum _currentBrowser;
+
+        //TODO: Change this 
+        private bool _headless = false;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            if (TestContext.CurrentContext.Test.Arguments.Length > 0)
+            {
+                _currentBrowser = (BrowserTypeEnum)TestContext.CurrentContext.Test.Arguments[0];
+                Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+
+                Browser = _currentBrowser switch
+                {
+                    BrowserTypeEnum.Chromium => await Playwright.Chromium.LaunchAsync(new() { Headless = _headless }),
+                    BrowserTypeEnum.Firefox => await Playwright.Firefox.LaunchAsync(new() { Headless = _headless }),
+                    BrowserTypeEnum.Webkit => await Playwright.Webkit.LaunchAsync(new() { Headless = _headless }),
+                    _ => throw new System.ArgumentOutOfRangeException()
+                };
+
+                Context = await Browser.NewContextAsync();
+                Page = await Context.NewPageAsync();
+            }
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Browser?.CloseAsync();
+            Playwright?.Dispose();
+        }
+    }
+}
