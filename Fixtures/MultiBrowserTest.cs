bd475e0812d9bf66dd771cbe524faefea516d255
Maria <mar1asm@yahoo.com>
2025-05-11 22:41:11 +0300

    Multi-browser testing improvement
    Improved multi-browser testing support — allows any test to run across all browsers with or without parameters

--- Fixtures/MultiBrowserTest.cs
+++ Fixtures/MultiBrowserTest.cs
@@ -1,29 +1,84 @@
 ﻿using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal.Builders;
 using NUnit.Framework.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tokero.Fixtures;
+using Tokero.TestData;
 
-namespace Tokero.Fixtures
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class MultiBrowserTest : NUnitAttribute, ITestBuilder
 {
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class MultiBrowserTest : NUnitAttribute, ITestBuilder
+    private readonly Type? _sourceType;
+
+    public MultiBrowserTest() { }
+
+    public MultiBrowserTest(Type sourceType)
     {
-        public IEnumerable<TestMethod> BuildFrom(IMethodInfo method, Test suite)
+        _sourceType = sourceType;
+    }
+
+    public IEnumerable<TestMethod> BuildFrom(IMethodInfo method, Test suite)
+    {
+        IEnumerable<object[]> testCases;
+
+        if (_sourceType != null)
+        {
+            var member = _sourceType.GetMethod(nameof(LanguageCases.LanguageTestCases));
+            if (member == null)
+                throw new InvalidOperationException($"TestCaseSource method not found in '{_sourceType.Name}'.");
+
+            var source = member.Invoke(null, null);
+
+            testCases = ExtractArguments(source);
+        }
+        else
+        {
+            testCases = new List<object[]> { Array.Empty<object>() };
+        }
+
+        var testCaseBuilder = new NUnitTestCaseBuilder();
+
+        foreach (var testCaseArgs in testCases)
         {
             foreach (BrowserTypeEnum browser in Enum.GetValues(typeof(BrowserTypeEnum)))
             {
-                var parameters = new TestCaseParameters(new object[] { browser });
+                var fullParams = new object[] { browser }.Concat(testCaseArgs).ToArray();
+                var parameters = new TestCaseParameters(fullParams);
+                var testMethod = testCaseBuilder.BuildTestMethod(method, suite, parameters);
 
-                var testMethod = new NUnitTestCaseBuilder().BuildTestMethod(method, suite, parameters);
-
-                testMethod.Name = $"{method.Name}_{browser}";
+                var argPart = testCaseArgs.Length > 0 ? $"_{string.Join("_", testCaseArgs)}" : "";
+                testMethod.Name = $"{method.Name}_{browser}{argPart}";
+                testMethod.Properties.Set(PropertyNames.Category, browser.ToString());
 
                 yield return testMethod;
             }
+        }
+    }
+
+    private static IEnumerable<object[]> ExtractArguments(object source)
+    {
+        if (source is IEnumerable<TestCaseData> testCaseData)
+        {
+            foreach (var data in testCaseData)
+                yield return data.Arguments;
+        }
+        else if (source is IEnumerable<object[]> objectArrays)
+        {
+            foreach (var array in objectArrays)
+                yield return array;
+        }
+        else if (source is IEnumerable<object> genericEnumerable)
+        {
+            foreach (var item in genericEnumerable)
+            {
+                if (item is object[] array)
+                    yield return array;
+                else
+                    yield return new[] { item };
+            }
+        }
+        else
+        {
+            throw new InvalidCastException("Test case source must be IEnumerable<TestCaseData> or IEnumerable<object[]>.");
         }
     }
 }
