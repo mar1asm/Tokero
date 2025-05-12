b171f1b60e8ecf8311f2c71fb2c6dc963880c049
Maria <mar1asm@yahoo.com>
2025-05-12 12:02:40 +0300

    Add LanguageTests
     testing language switcher functionality

---
+++ Tests/Home/HomeLanguageTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokero.Fixtures;
+using Tokero.Pages;
+using Tokero.TestData;
+using TokeroTests.Fixtures;
+
+namespace Tokero.Tests.Home
+{
+    public class HomeLanguageTests: PlaywrightTestFixture
+    {
+        [Test, MultiBrowserTest(typeof(LanguageCases))]
+
+        public async Task LanguageSwitch_ShowsExpectedWord(BrowserTypeEnum browser, string langCode, string expectedWord)
+        {
+            var homePage = new HomePage(Page);
+
+            await homePage.GoToAsync();
+            await homePage.AcceptCookiesAsync();
+
+            await homePage.SelectLanguageAsync(langCode);
+
+            var visible = await homePage.IsKeywordVisibleAsync(expectedWord);
+
+            Assert.IsTrue(visible, $"Expected '{expectedWord}' for '{langCode}' in {browser}");
+        }
+    }
+}
