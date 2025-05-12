ccf7f3a67a206685cb699ad2981b07ddb2ad7ef8
Maria <mar1asm@yahoo.com>
2025-05-12 17:18:08 +0300

    Add Exchange page tests for default and switched parity behavior

---
+++ Tests/Exchange/ExchangeTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokero.Fixtures;
+using Tokero.Pages;
+using TokeroTests.Fixtures;
+
+namespace Tokero.Tests.Exchange
+{
+    public class ExchangeTests : PlaywrightTestFixture
+    {
+        [Test, MultiBrowserTest]
+        public async Task Exchange_DefaultParityIsEUR(BrowserTypeEnum browser)
+        {
+            var exchange = new ExchangePage(Page);
+            await exchange.GoToAsync();
+
+            var parity = await exchange.GetSelectedParityAsync();
+            Assert.That(parity, Is.EqualTo("EUR"));
+        }
+
+        [Test, MultiBrowserTest]
+        public async Task Exchange_SwitchParity_UpdatesTable(BrowserTypeEnum browser)
+        {
+            var exchange = new ExchangePage(Page);
+            await exchange.GoToAsync();
+
+            await exchange.SelectParityAsync("USD");
+            Assert.That(await exchange.GetSelectedParityAsync(), Is.EqualTo("USD"));
+            var a = await exchange.DataTableRows.CountAsync();
+            Assert.That(await exchange.DataTableRows.CountAsync() > 0);
+        }
+    }
+}
