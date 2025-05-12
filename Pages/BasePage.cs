4198e00df08c788433f35a5b13970c30224d1bba
Maria <mar1asm@yahoo.com>
2025-05-12 11:08:43 +0300

    Create BasePage (POM)
     Provide common functionality for page interactions in tests

---
+++ Pages/BasePage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokero.Utils;
+
+namespace Tokero.Pages
+{
+    public abstract class BasePage
+    {
+        protected readonly IPage _page;
+        protected abstract string PageRoute { get; }
+
+        protected BasePage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task GoToAsync()
+        {
+            await _page.GotoAsync($"{TestConfig.BaseUrl}/{PageRoute}".TrimEnd('/'));
+        }
+    }
+}
