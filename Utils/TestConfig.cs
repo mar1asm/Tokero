1c9cf4577ec302a3e981152c288556561e7f68f4
Maria <mar1asm@yahoo.com>
2025-05-12 11:06:59 +0300

    Create testConfig file
     Store generic test configuration parameters for easier management

---
+++ Utils/TestConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokero.Utils
+{
+    public static class TestConfig
+    {
+        public static string BaseUrl { get; set; } = "https://tokero.com";
+        public static string DefaultLanguage { get; set; } = "EN";
+        public static bool IsHeadless { get; set; } = true;
+        public static int DefaultTimeout { get; set; } = 5000;
+    }
+}
